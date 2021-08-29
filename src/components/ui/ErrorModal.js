5429e60fbc39c9dc8b3c403d9181ec95677e7b11
Yonas Gashaw <YGashaw@excellerentsolutions.com>
2021-08-29 00:09:36 -0700

    using react portals

--- src/components/ui/ErrorModal.js
+++ src/components/ui/ErrorModal.js
@@ -1,24 +1,38 @@
 import React from 'react'
+import reactDom from 'react-dom'
 import Button from './Button'
 import Card from './Card'
 import styles from './errorModal.module.css'
 
+const Backdrop = props => {
+    return (
+        <div className={styles.backdrop} onClick={props.onConfirm} />
+    )
+}
+
+const ModalOverlay = props => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
+
 const ErrorModal = (props) => {
     return (
-        <>
-            <div className={styles.backdrop} onClick={props.onConfirm} />
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-        </>
+        /* <> ... </> also works fine in place of <Fragment>...</Fragment>. */
+        <React.Fragment>
+            {reactDom.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {reactDom.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))}
+        </React.Fragment>
     )
 }
 
