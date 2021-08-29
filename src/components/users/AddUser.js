040d2ac59e3d1f04b2fe4be688eeb3bd7a4bcd5c
Yonas Gashaw <YGashaw@excellerentsolutions.com>
2021-08-29 11:52:19 -0700

    adding and using refs - the useRef hook

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,68 +1,90 @@
-import React, { useState } from 'react'
-import Wrapper from '../helpers/Wrapper'
-import Button from '../ui/Button'
-import Card from '../ui/Card'
-import ErrorModal from '../ui/ErrorModal'
-import styles from './addUser.module.css'
+import React, { useState, useRef } from "react";
+import Wrapper from "../helpers/Wrapper";
+import Button from "../ui/Button";
+import Card from "../ui/Card";
+import ErrorModal from "../ui/ErrorModal";
+import styles from "./addUser.module.css";
 
 const AddUser = (props) => {
-    const [enteredUserName, setEnteredUserName] = useState('')
-    const [enteredAge, setEnteredAge] = useState('')
-    const [error, setError] = useState()
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
-    const addUserHandler = (event) => {
-        event.preventDefault()
-        if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
-            setError({
-                title: 'Invalid input',
-                message: 'Please enter a valid name and age (non-empty values).'
-            })
-            return
-        }
-        if (+enteredAge < 18) {
-            /* the reason why we add '+' is just
+  /* the below lines are being used when we use state - useState */
+  //   const [enteredUserName, setEnteredUserName] = useState("");
+  //   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    // console.log(nameInputRef.current.value)
+    const enteredUName = nameInputRef.current.value;
+    const enteredUAge = ageInputRef.current.value;
+
+    if (enteredUName.trim().length === 0 || enteredUAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
+    if (+enteredUAge < 18) {
+      /* the reason why we add '+' is just
                 to force the variable being used 
                 to cast it to number.
             */
-            setError({
-                title: 'Invalid age',
-                message: 'Please enter a valid age (> 18).'
-            })
-            return
-        }
-        // console.log(enteredUserName, enteredAge)
-        props.onAddUser(enteredUserName, enteredAge)
-        setEnteredUserName('')
-        setEnteredAge('')
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 18).",
+      });
+      return;
     }
+    // console.log(enteredUserName, enteredAge)
+    props.onAddUser(enteredUName, enteredUAge);
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUserName(event.target.value)
-    }
+    /* though it's not recommended to manipulate the DOM elements
+        we can easily reset the field values like as below */
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
 
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value)
-    }
+    /* the below lines are being used when we use state - useState */
+    // props.onAddUser(enteredUserName, enteredAge);
+    // setEnteredUserName("");
+    // setEnteredAge("");
+  };
 
-    const errorHandler = () => {
-        setError(null)
-    }
+  //   const usernameChangeHandler = (event) => {
+  //     setEnteredUserName(event.target.value);
+  //   };
 
-    return (
-        <Wrapper>
-            {error && (<ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />)}
-            <Card className={styles.input}>
-                <form onSubmit={addUserHandler}>
-                    <label htmlFor='userName'>Username</label>
-                    <input id='username' type='text' value={enteredUserName} onChange={usernameChangeHandler} />
-                    <label htmlFor='age'>Age (Years)</label>
-                    <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler} />
-                    {/* <button type='submit'>Add User</button> */}
-                    <Button type='submit'>Add User</Button>
-                </form>
-            </Card>
-        </Wrapper>
-    )
-}
+  //   const ageChangeHandler = (event) => {
+  //     setEnteredAge(event.target.value);
+  //   };
 
-export default AddUser
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="userName">Username</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputRef} />
+          {/* <button type='submit'>Add User</button> */}
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
